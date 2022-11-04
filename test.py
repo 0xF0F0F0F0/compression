82bc0e7da367c9a8e29f5875a8e0643c006f11b2
0xF0F0F0F0 <ayush.bhat.666@gmail.com>
2022-11-04 14:31:32 +0100

    plots

--- test.py
+++ test.py
@@ -1,22 +1,18 @@
 import queue
 import numpy as np
 import pyflac
-import soundfile as sf
+import matplotlib.pyplot as plt
 
 idx = 0
 total_bytes = 0
 rate = 44100
 data_queue = queue.SimpleQueue()
-fid = open('decoded.txt')
 tmp = np.array
 
 def write_callback(buf: bytes, num_bytes: int, num_samples: int, frame_num: int):
     global total_bytes
     total_bytes += num_bytes
     data_queue.put(buf)
-    #print(frame_num)
-    #print(buf, '\n')
-
 
 def read_callback(decoded_data: np.array, sample_rate: int,
                   num_channels: int, num_samples: int):
@@ -25,7 +21,8 @@ def read_callback(decoded_data: np.array, sample_rate: int,
     idx += num_samples
     tmp = decoded_data 
 
-data = np.fromfile('coeff3.raw', dtype=np.int16, sep='\n')
+#data = np.fromfile('coeff.txt', dtype=np.int16, sep='\n')
+data = np.fromfile('coeff3.txt', dtype=np.int16, sep='\n')
 
 encoder = pyflac.StreamEncoder(rate, write_callback, blocksize = 0, verify=True)
 encoder.process(data)
@@ -44,4 +41,14 @@ if(np.array_equal(tmp, data)):
 print('\nUncompressed: ' +  str(data.nbytes) + ' bytes\n')
 print('Compressed: ' +  str(total_bytes) + ' bytes\n')
 print('Ratio: {ratio:.2f}%'.format(ratio= (1-(total_bytes / data.nbytes) )* 100))
+
+names = ['RAW', 'FLAC']
+values = [data.nbytes, total_bytes]
+
+plt.bar(names, values)
+plt.title('Bytes Compressed Using FLAC')
+plt.ylabel('Bytes')
+plt.show()
+
+
 
